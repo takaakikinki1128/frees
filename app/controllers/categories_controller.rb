f613a1fbda992832955dffbb4338ac2f87ed8bb1
takaakikinki1128 <takaakikinki1128@icloud.com>
2019-07-22 04:39:56 +0900

    ok

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
     @categories1 = params.require(:category)[:keywords]
     @genre = []
     @categories1.each do |category|
-      @genre2 = Tweet.where("category_id = ?",category) 
+      @genre2 = Tweet.where("category_id = ?",category)
       @genre << @genre2
     end
   end
