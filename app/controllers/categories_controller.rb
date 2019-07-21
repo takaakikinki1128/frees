8c0b03089f26ee4e8641ee7c6a74d382115cef61
takaakikinki1128 <takaakikinki1128@icloud.com>
2019-07-22 01:58:50 +0900

    miss

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
     @categories1 = params.require(:category)[:keywords]
     @genre = []
     @categories1.each do |category|
-      @genre2 = Tweet.where("category_id = ?",category) 
+      @genre2 = Tweet.where("category_id = ?",category).page(params[:page]).per(3).order("created_at DESC")
       @genre << @genre2
     end
   end
